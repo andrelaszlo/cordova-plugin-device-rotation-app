41f9eb2245c8d6202d55bd27b426571efd00b207
André Laszlo <andre@laszlo.nu>
2018-03-21 19:10:34 +0100

    Use npm version of Cordova device rotation plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var CordovaPluginDeviceRotation: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public orientation: any;
 
+  constructor(public navCtrl: NavController, ref: ChangeDetectorRef) {
+    this.orientation = "Loading...";
+    CordovaPluginDeviceRotation.onOrientationChange(orientation => {
+      console.log("New orientation", orientation);
+      this.orientation = orientation;
+      ref.detectChanges();
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('view loaded');
   }
 
 }
